a8030df16004efc9bb85ec68e7b82d5a3caf6326
N3croide <106851453+N3croide@users.noreply.github.com>
2023-12-20 17:50:28 -0500

    primer ejercicio practico

---
+++ ejerciciosPrincipiantes/primero/README.md
@@ -0,0 +1,13 @@
+# 		PRIMER EJERCICIO PRACTICO
+
+
+
+| VARIABLE | E/S  | TIPO DE DATO |
+| :------: | :--: | :----------: |
+|  numero  |  E   |     INT      |
+|          |      |              |
+
+En el main lo unico que haremos es mediante un for recorrerlo con la funcion range() desde 0 hasta 3 sin incluir el 3, crearemos dos funciones:
+
+1. pedirNum(): Pedira el numero y comprobara que se un numero entero ya sea  positivo o negativo y llamara a la funcion para comprobar si el numero es positivo o negativo y si es positivo retornara el valor el cual estamos almacenando en la variable numero
+2. comprobarNum(): este metodo lo que hara es pedir el numero que el usuario acaba de ingresar y mediante un condicional if verificamos si es mayor a cero y retorna True si es mayor a 0 o False si es el caso contrario
---
+++ ejerciciosPrincipiantes/primero/primero.py
@@ -0,0 +1,30 @@
+import os
+#Metodo para exclusivamente para comprobar que el numero sea positivo
+def comprobarNum(numero) -> bool:
+    if (numero > 0):
+       return True
+    else:
+        return False
+    
+#Metodo que va a retornar el valor solo cuando el valor sea un numero entero positivo
+def pedirNum() -> int:
+    while(True):    
+        try:
+            numero = int(input("Ingrese un numero entero positivo: "))
+        except ValueError:
+            print("Porfavor ingrese un numero entero")
+        else:
+            if(comprobarNum(numero)):
+                return numero
+            else:
+                os.system("cls")
+                print("Tiene que ingresar un numero positivo, recuerde que el cero no es no positivo ni negativo.")
+
+
+#MAIN FUNCTION
+numero = 0
+os.system("cls")
+for i in range(0,3):
+    numero += pedirNum()
+print(numero)
+
